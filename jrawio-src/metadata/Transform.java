ab1e4efe15147ea78bb4a9664a97993b3adfd7f4
Fabrizio Giudici <Fabrizio.Giudici@tidalwave.it>
2006-11-28 13:43:27 +0100

    [svn r9] License changed to ASF 2.0 in headers.

--- jrawio-src/metadata/Transform.java
+++ jrawio-src/metadata/Transform.java
@@ -1,3 +1,30 @@
+/*******************************************************************************
+ *
+ * jrawio - a Java(TM) ImageIO API Spi Provider for RAW files
+ * ==========================================================
+ *
+ * Copyright (C) 2003-2006 by Fabrizio Giudici (Fabrizio.Giudici@tidalwave.it)
+ * Project home page: http://jrawio.tidalwave.it
+ *
+ *******************************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0 
+ * 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License. 
+ *
+ *******************************************************************************
+ *
+ * $Id: Transform.java 9 2006-11-28 12:43:27Z fabriziogiudici $
+ *
+ ******************************************************************************/
 import javax.xml.parsers.*;
 import javax.xml.transform.*;
 import javax.xml.transform.dom.*; 
--- reajent-src/src/it/tidalwave/imageio/rawprocessor/cr2/CR2CurveOperation.java
+++ reajent-src/src/it/tidalwave/imageio/rawprocessor/cr2/CR2CurveOperation.java
@@ -1,36 +1,28 @@
 /*******************************************************************************
  *
  * jrawio - a Java(TM) ImageIO API Spi Provider for RAW files
- * ----------------------------------------------------------
+ * ==========================================================
  *
  * Copyright (C) 2003-2006 by Fabrizio Giudici (Fabrizio.Giudici@tidalwave.it)
- * Project home page: http://jrawio.dev.java.net
- * 
+ * Project home page: http://jrawio.tidalwave.it
+ *
  *******************************************************************************
  *
- * MIT License notice
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
  * 
- * Permission is hereby granted, free of charge, to any person obtaining a copy 
- * of this software and associated documentation files (the "Software"), to deal 
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
+ *     http://www.apache.org/licenses/LICENSE-2.0 
  * 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License. 
+ *
  *******************************************************************************
  *
- * $Id: CR2CurveOperation.java,v 1.1 2006/02/17 15:32:08 fabriziogiudici Exp $
+ * $Id: CR2CurveOperation.java 9 2006-11-28 12:43:27Z fabriziogiudici $
  *
  ******************************************************************************/
 package it.tidalwave.imageio.rawprocessor.cr2;
@@ -43,7 +35,7 @@ import it.tidalwave.imageio.rawprocessor.raw.CurveOperation;
 /*******************************************************************************
  *
  * @author  Fabrizio Giudici
- * @version CVS $Id: CR2CurveOperation.java,v 1.1 2006/02/17 15:32:08 fabriziogiudici Exp $
+ * @version CVS $Id: CR2CurveOperation.java 9 2006-11-28 12:43:27Z fabriziogiudici $
  *
  ******************************************************************************/
 public class CR2CurveOperation extends CurveOperation  
