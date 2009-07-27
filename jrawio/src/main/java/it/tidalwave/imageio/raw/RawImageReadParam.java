5c5552827fbd696405407c64a3e2d7da0d7e779e
Fabrizio Giudici <Fabrizio.Giudici@tidalwave.it>
2009-07-27 15:16:27 +0200

    Added RawImageReadParam.

---
+++ jrawio/src/main/java/it/tidalwave/imageio/raw/RawImageReadParam.java
@@ -0,0 +1,40 @@
+/*******************************************************************************
+ *
+ * jrawio - a Java(TM) ImageIO API Spi Provider for RAW files
+ * ==========================================================
+ *
+ * Copyright (C) 2003-2009 by Fabrizio Giudici
+ * Project home page: http://jrawio.tidalwave.it
+ *
+ *******************************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0 
+ * 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License. 
+ *
+ *******************************************************************************
+ *
+ * $Id: Version.java 263 2009-06-15 09:18:57Z fabriziogiudici $
+ *
+ ******************************************************************************/
+package it.tidalwave.imageio.raw;
+
+import javax.imageio.ImageReadParam;
+
+/*******************************************************************************
+ *
+ * @author  Fabrizio Giudici
+ * @version $Id: Version.java 263 2009-06-15 09:18:57Z fabriziogiudici $
+ *
+ ******************************************************************************/
+public class RawImageReadParam extends ImageReadParam
+  {
+  }
