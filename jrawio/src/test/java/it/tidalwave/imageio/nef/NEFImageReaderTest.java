5766e0fffdc0943b871bc30366654f7c224d00b9
Fabrizio Giudici <Fabrizio.Giudici@tidalwave.it>
2009-08-06 01:11:52 +0200

    Fix for JRW-196.

--- jrawio/src/test/java/it/tidalwave/imageio/nef/NEFImageReaderTest.java
+++ jrawio/src/test/java/it/tidalwave/imageio/nef/NEFImageReaderTest.java
@@ -120,6 +120,158 @@ public class NEFImageReaderTest extends ImageReaderTestSupport
       }
 
     @Test(timeout=60000)
+    public void testNikonD70_1_0_1()
+      throws Exception
+      {
+        final String path = "http://s179771984.onlinehome.us/RAWpository/images/nikon/D70/1.01/DSC_2544.NEF";
+        final ImageReader ir = getImageReader(path);
+        assertEquals(1, ir.getNumImages(false));
+        assertEquals(2, ir.getNumThumbnails(0));
+        assertImage(ir, 3040, 2014);
+        assertThumbnail(ir, 0, 160, 120);
+        assertThumbnail(ir, 1, 3008, 2000);
+        final BufferedImage image = assertLoadImage(ir, 3040, 2014, 3, 16);
+        assertLoadThumbnail(ir, 0, 160, 120);
+        assertLoadThumbnail(ir, 1, 3008, 2000);
+        close(ir);
+
+        assertRaster(image, path, "f5d7d487f69fee3ef0b6b344729cf2cf");
+      }
+
+    @Test(timeout=60000)
+    public void testNikonD70_1_0_2()
+      throws Exception
+      {
+        final String path = "http://s179771984.onlinehome.us/RAWpository/images/nikon/D70/1.02/DSC_1945.NEF";
+        final ImageReader ir = getImageReader(path);
+        assertEquals(1, ir.getNumImages(false));
+        assertEquals(2, ir.getNumThumbnails(0));
+        assertImage(ir, 3040, 2014);
+        assertThumbnail(ir, 0, 160, 120);
+        assertThumbnail(ir, 1, 3008, 2000);
+        final BufferedImage image = assertLoadImage(ir, 3040, 2014, 3, 16);
+        assertLoadThumbnail(ir, 0, 160, 120);
+        assertLoadThumbnail(ir, 1, 3008, 2000);
+        close(ir);
+
+        assertRaster(image, path, "e6f4da41f81d0565509b84d89f0d678e");
+      }
+
+    @Test(timeout=60000)
+    public void testNikonD70_2_0_0()
+      throws Exception
+      {
+        final String path = "http://s179771984.onlinehome.us/RAWpository/images/nikon/D70/2.00/_DSC4798.NEF";
+        final ImageReader ir = getImageReader(path);
+        assertEquals(1, ir.getNumImages(false));
+        assertEquals(2, ir.getNumThumbnails(0));
+        assertImage(ir, 3040, 2014);
+        assertThumbnail(ir, 0, 160, 120);
+        assertThumbnail(ir, 1, 3008, 2000);
+        final BufferedImage image = assertLoadImage(ir, 3040, 2014, 3, 16);
+        assertLoadThumbnail(ir, 0, 160, 120);
+        assertLoadThumbnail(ir, 1, 3008, 2000);
+        close(ir);
+
+        assertRaster(image, path, "4cd1e5dc546289ced51b97996a04893f");
+      }
+
+    @Test(timeout=60000)
+    public void testNikonD40_1_0_0()
+      throws Exception
+      {
+        final String path = "http://s179771984.onlinehome.us/RAWpository/images/nikon/D40/1.00/DSC_0108.NEF";
+        final ImageReader ir = getImageReader(path);
+        assertEquals(1, ir.getNumImages(false));
+        assertEquals(2, ir.getNumThumbnails(0));
+        assertImage(ir, 3040, 2014);
+        assertThumbnail(ir, 0, 160, 120);
+        assertThumbnail(ir, 1, 3008, 2000);
+        final BufferedImage image = assertLoadImage(ir, 3040, 2014, 3, 16);
+        assertLoadThumbnail(ir, 0, 160, 120);
+        assertLoadThumbnail(ir, 1, 3008, 2000);
+        close(ir);
+
+        assertRaster(image, path, "3b565723bb5b3c7db33fc2e69cca040c");
+      }
+
+    @Test(timeout=60000)
+    public void testNikonD50_1_0_0()
+      throws Exception
+      {
+        final String path = "http://s179771984.onlinehome.us/RAWpository/images/nikon/D50/1.00/DSC_0015.NEF";
+        final ImageReader ir = getImageReader(path);
+        assertEquals(1, ir.getNumImages(false));
+        assertEquals(2, ir.getNumThumbnails(0));
+        assertImage(ir, 3040, 2014);
+        assertThumbnail(ir, 0, 160, 120);
+        assertThumbnail(ir, 1, 3008, 2000);
+        final BufferedImage image = assertLoadImage(ir, 3040, 2014, 3, 16);
+        assertLoadThumbnail(ir, 0, 160, 120);
+        assertLoadThumbnail(ir, 1, 3008, 2000);
+        close(ir);
+
+        assertRaster(image, path, "067d8ab1983f4e8801f13046fe426baf");
+      }
+
+    @Test(timeout=60000)
+    public void testNikonD2X_1_0_1()
+      throws Exception
+      {
+        final String path = "http://s179771984.onlinehome.us/RAWpository/images/nikon/D2X/1.01/_DSC0733.NEF";
+        final ImageReader ir = getImageReader(path);
+        assertEquals(1, ir.getNumImages(false));
+        assertEquals(2, ir.getNumThumbnails(0));
+        assertImage(ir, 4320, 2868);
+        assertThumbnail(ir, 0, 160, 120);
+        assertThumbnail(ir, 1, 4288, 2848); 
+        final BufferedImage image = assertLoadImage(ir, 4320, 2868, 3, 16);
+        assertLoadThumbnail(ir, 0, 160, 120);
+        assertLoadThumbnail(ir, 1, 4288, 2848);
+        close(ir);
+
+        assertRaster(image, path, "0cb29a0834bb2293ee4bf0c09b201631");
+      }
+
+    @Test(timeout=60000)
+    public void testNikonD2Xs_1_0_0()
+      throws Exception
+      {
+        final String path = "http://s179771984.onlinehome.us/RAWpository/images/nikon/D2Xs/1.00/DSC_1234.nef";
+        final ImageReader ir = getImageReader(path);
+        assertEquals(1, ir.getNumImages(false));
+        assertEquals(2, ir.getNumThumbnails(0));
+        assertImage(ir, 4320, 2868);
+        assertThumbnail(ir, 0, 160, 120);
+        assertThumbnail(ir, 1, 4288, 2848); 
+        final BufferedImage image = assertLoadImage(ir, 4320, 2868, 3, 16);
+        assertLoadThumbnail(ir, 0, 160, 120);
+        assertLoadThumbnail(ir, 1, 4288, 2848);
+        close(ir);
+
+        assertRaster(image, path, "37d5aa7aab4e2d4fd667efb674f558ed");
+      }
+
+//    @Test(timeout=60000)
+//    public void testNikonD80()
+//      throws Exception
+//      {
+//        final String path = "http://rapidshare.de/files/31631976/DSC_0001.NEF";
+//        final ImageReader ir = getImageReader(path);
+//        assertEquals(1, ir.getNumImages(false));
+//        assertEquals(2, ir.getNumThumbnails(0));
+//        assertImage(ir, 3040, 2014);
+//        assertThumbnail(ir, 0, 106, 160);
+//        assertThumbnail(ir, 1, 384, 255);
+//        final BufferedImage image = assertLoadImage(ir, 3040, 2014, 3, 16);
+//        assertLoadThumbnail(ir, 0, 106, 160);
+//        assertLoadThumbnail(ir, 1, 384, 255);
+//        close(ir);
+//
+//        assertRaster(image, path, "fabdb471b9a972acf313ebe29bf338ea");
+//      }
+
+    @Test(timeout=60000)
     public void testNikonD200()
       throws Exception
       {
@@ -168,7 +320,7 @@ public class NEFImageReaderTest extends ImageReaderTestSupport
         assertImage(ir, 4352, 2868);
         assertThumbnail(ir, 0, 160, 120);
         assertThumbnail(ir, 1, 4288, 2848);
-        final BufferedImage image = assertLoadImage(ir, 4352, 2868, 3, 16, 0 /*BufferedImage.TYPE_INT_RGB*/); // FIXME: WRONG, should be 16 bits
+        final BufferedImage image = assertLoadImage(ir, 4352, 2868, 3, 16, 0 /*BufferedImage.TYPE_INT_RGB*/);
         assertLoadThumbnail(ir, 0, 160, 120);
         assertLoadThumbnail(ir, 1, 4288, 2848);
         close(ir);
