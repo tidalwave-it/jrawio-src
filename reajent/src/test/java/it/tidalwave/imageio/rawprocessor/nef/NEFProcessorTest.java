6c30bd5add7b72734c8136fe8e0279ab9eb384a1
Fabrizio Giudici <Fabrizio.Giudici@tidalwave.it>
2009-08-08 11:22:10 +0200

    Updated fingerprints.

--- reajent/src/test/java/it/tidalwave/imageio/rawprocessor/nef/NEFProcessorTest.java
+++ reajent/src/test/java/it/tidalwave/imageio/rawprocessor/nef/NEFProcessorTest.java
@@ -54,60 +54,62 @@ public class NEFProcessorTest  extends NewImageReaderTestSupport
           (
             // D2X v1.0.1
             ExpectedResults.create("http://s179771984.onlinehome.us/RAWpository/images/nikon/D2X/1.01/_DSC0733.NEF").
-                            image(4288, 2848, 3, 8, "0cb29a0834bb2293ee4bf0c09b201631").
+                            image(4288, 2848, 3, 8, "f0c8a971dc82b31f1de739d7fb3d8a10").
                             thumbnail(160, 120).
                             thumbnail(4288, 2848),
             // D2Xs v1.0.0
             ExpectedResults.create("http://s179771984.onlinehome.us/RAWpository/images/nikon/D2Xs/1.00/DSC_1234.nef").
-                            image(4288, 2848, 3, 8, "37d5aa7aab4e2d4fd667efb674f558ed").
+                            image(4288, 2848, 3, 8, "f7d94c239bcc9bf2c8fd1b180049b401").
                             thumbnail(160, 120).
-                            thumbnail(4288, 2848),
+                            thumbnail(4288, 2848).
+                            issues("JRW-215"),
             // D40 v1.0.0
             ExpectedResults.create("http://s179771984.onlinehome.us/RAWpository/images/nikon/D40/1.00/DSC_0108.NEF").
-                            image(3008, 2000, 3, 8, "3b565723bb5b3c7db33fc2e69cca040c").
+                            image(2000, 3008, 3, 8, "51f5ec81eff835ac17061b050f247fe5").
                             thumbnail(160, 120).
-                            thumbnail(3008, 2000),
+                            thumbnail(3008, 2000).
+                            issues("JRW-215"),
             // D50 v1.0.0
             ExpectedResults.create("http://s179771984.onlinehome.us/RAWpository/images/nikon/D50/1.00/DSC_0015.NEF").
-                            image(3008, 2000, 3, 8, "067d8ab1983f4e8801f13046fe426baf").
+                            image(3008, 2000, 3, 8, "5b785ffd3a34430c58e56ea5b204d7d3").
                             thumbnail(160, 120).
                             thumbnail(3008, 2000),
             // D50
             ExpectedResults.create("https://imaging.dev.java.net/nonav/TestSets/others/konstantinmaslov/Nikon/D50/NEF/France_Collioure_1.NEF").
-                            image(2000, 3008, 3, 8, "328f3bdabe9ba80a3e4abe7237fa4084").
+                            image(2000, 3008, 3, 8, "3a5207518665f8a39abfbee7d5b39a1b").
                             thumbnail(160, 120).
                             thumbnail(3008, 2000).
                             issues("JRW-129"),
             // D70 v1.0.1
             ExpectedResults.create("http://s179771984.onlinehome.us/RAWpository/images/nikon/D70/1.01/DSC_2544.NEF").
-                            image(3008, 2000, 3, 8, "f5d7d487f69fee3ef0b6b344729cf2cf").
+                            image(3008, 2000, 3, 8, "72f0034d835dbbc0afe9d65255de9423").
                             thumbnail(160, 120).
                             thumbnail(3008, 2000),
             // D70 v1.0.2
             ExpectedResults.create("http://s179771984.onlinehome.us/RAWpository/images/nikon/D70/1.02/DSC_1945.NEF").
-                            image(3008, 2000, 3, 8, "e6f4da41f81d0565509b84d89f0d678e").
+                            image(2000, 3008, 3, 8, "2a431f6635b9f51b33aab9854a3c5dac").
                             thumbnail(160, 120).
                             thumbnail(3008, 2000),
             // D70 v2.0.0
             ExpectedResults.create("http://s179771984.onlinehome.us/RAWpository/images/nikon/D70/2.00/_DSC4798.NEF").
-                            image(3008, 2000, 3, 8, "4cd1e5dc546289ced51b97996a04893f").
+                            image(3008, 2000, 3, 8, "7cdf5a744af7bf39d6988dfc29f5bb17").
                             thumbnail(160, 120).
                             thumbnail(3008, 2000),
             // D70s
             ExpectedResults.create("https://imaging.dev.java.net/nonav/TestSets/mpetersen/Nikon/D70s/NEF/Nikon_D70s_0001.NEF").
-                            image(2000, 3008, 3, 8, "d62309eab197bb9aced6a56e5fa966a9").
+                            image(2000, 3008, 3, 8, "5f72336ecb735433fb379fcc830b6ae6").
                             thumbnail(106, 160).
                             thumbnail(384, 255).
                             issues("JRW-149", "JRW-150"),
             // D90
             ExpectedResults.create("http://jalbum.net/download/DSC_0067.NEF").
-                            image(4288, 2848, 3, 8, "a23d77ce9ad5e6a666407c4230ce49e9").
+                            image(4288, 2848, 3, 8, "f2bced57381390ff015bf0fe671fdeab").
                             thumbnail(160, 120).
                             thumbnail(4288, 2848).
-                            issues("JRW-187"),
+                            issues("JRW-187", "JRW-215"),
             // D100
             ExpectedResults.create("https://imaging.dev.java.net/nonav/TestSets/fabriziogiudici/Nikon/D100/NEF/NikonCaptureEditor/ccw90.nef").
-                            image(2000, 3008, 3, 8, "6e7c4edc6a5389ab6f0887ee5a6f7527").
+                            image(2000, 3008, 3, 8, "f9fab4ee10aa950bfa30426783ea3fda").
                             thumbnail(120, 160).
                             issues("JRW-146"),
             // D100
@@ -117,12 +119,12 @@ public class NEFProcessorTest  extends NewImageReaderTestSupport
                             issues("JRW-148"),
             // D200
             ExpectedResults.create("https://imaging.dev.java.net/nonav/TestSets/fabriziogiudici/Nikon/D200/NEF/20080427-0029.NEF").
-                            image(3872, 2592, 3, 8, "fabdb471b9a972acf313ebe29bf338ea").
+                            image(3872, 2592, 3, 8, "05845e10ae91f7870c51e98a28d4ad80").
                             thumbnail(160, 120).
                             thumbnail(3872, 2592),
             // D200
             ExpectedResults.create("http://s179771984.onlinehome.us/RAWpository/images/nikon/D200/1.00/DCC_0280.NEF").
-                            image(3872, 2592, 3, 8, "eca27d2b7ade85c4803f287b8c413844").
+                            image(3872, 2592, 3, 8, "6775afb3b9401950a7afe301497acd07").
                             thumbnail(160, 120).
                             thumbnail(3872, 2592)
           );
