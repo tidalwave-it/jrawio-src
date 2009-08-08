03eefe9ad349c2ac7e7bb200831e1d02f0bfabc2
Fabrizio Giudici <Fabrizio.Giudici@tidalwave.it>
2009-08-08 22:27:49 +0200

    Method RawMetadataSupport.setSize() -> RawMetadataSupport.setOverriddenImageSize(), improved logging (fix for JRW-224).

--- reajent/src/main/java/it/tidalwave/imageio/rawprocessor/raw/SizeOperation.java
+++ reajent/src/main/java/it/tidalwave/imageio/rawprocessor/raw/SizeOperation.java
@@ -123,7 +123,7 @@ public abstract class SizeOperation extends OperationSupport
       {
         logger.fine("processMetadata(%s)", artifact);
         final int rotation = normalizedAngle(artifact.getRotation());
-        artifact.getRAWMetadata().setSize(rotate(getSize(artifact), rotation));
+        artifact.getRAWMetadata().setOverriddenImageSize(rotate(getSize(artifact), rotation));
       } 
     
     /*******************************************************************************************************************
@@ -153,7 +153,7 @@ public abstract class SizeOperation extends OperationSupport
     @Nonnull
     protected Dimension getSize (@Nonnull final PipelineArtifact artifact)
       {
-        logger.fine("getSize()");
+        logger.fine("getSize(%s)", artifact);
         final TIFFMetadataSupport metadata = (TIFFMetadataSupport)artifact.getRAWMetadata();
         final IFD primaryIFD = metadata.getPrimaryIFD();
         return getStandardSize(primaryIFD.getModel());
