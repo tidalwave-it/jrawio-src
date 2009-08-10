91317768a18fa8642749d815b4cbfc549afd0397
Fabrizio Giudici <Fabrizio.Giudici@tidalwave.it>
2009-08-10 14:46:45 +0200

    Integration tests merged into CRWProcessorTest.

--- reajent/src/test/java/it/tidalwave/imageio/rawprocessor/crw/CRWProcessorTest.java
+++ reajent/src/test/java/it/tidalwave/imageio/rawprocessor/crw/CRWProcessorTest.java
@@ -31,6 +31,8 @@ import javax.annotation.Nonnull;
 import java.util.Collection;
 import it.tidalwave.imageio.ExpectedResults;
 import it.tidalwave.imageio.NewImageReaderTestSupport;
+import it.tidalwave.imageio.raw.RAWImageReadParam;
+import it.tidalwave.imageio.raw.Source;
 import org.junit.runners.Parameterized.Parameters;
 
 /***********************************************************************************************************************
@@ -56,7 +58,41 @@ public class CRWProcessorTest extends NewImageReaderTestSupport
             ExpectedResults.create("https://imaging.dev.java.net/nonav/TestSets/esordini/Canon/EOS300D/CRW/100_0056.CRW").
                             image(3072, 2048, 3, 8, "67994f630322d2bede87a2884f5f0965").
                             thumbnail(2048, 1360).
-                            issues("JRW-104", "JRW-218", "JRW-219")
-          );
+                            issues("JRW-104", "JRW-218", "JRW-219"),
+            ExpectedResults.create("https://imaging.dev.java.net/nonav/TestSets/esordini/Canon/EOS300D/CRW/100_0056.CRW").
+                            param(new RAWImageReadParam(Source.RAW_IMAGE)).
+                            image(3152, 2068, 3, 16, "b499a77c82e5289d043e2f330c6fffba").
+                            thumbnail(2048, 1360).
+                            issues("JRW-104", "JRW-200").
+                            metadata("metadata.width", 3152).
+                            metadata("metadata.height", 2068).
+                            metadata("metadata.fileNumber", 1000056).
+                            metadata("metadata.imageWidth", 3072).
+                            metadata("metadata.imageHeight", 2048).
+                            metadata("metadata.thumbnailWidth", 2048).
+                            metadata("metadata.thumbnailHeight", 1360).
+                            metadata("metadata.pixelAspectRatio", 1.0f).
+                            metadata("metadata.rotation", 0).
+                            metadata("metadata.componentBitDepth", 8).
+                            metadata("metadata.colorBitDepth", 24).
+                            metadata("metadata.colorBW", 257).
+//                            metadata("metadata.timeStampAsDate", 0).
+                            metadata("metadata.baseISO", 100).
+                            metadata("metadata.firmwareVersion", "Firmware Version 1.1.1").
+                            metadata("metadata.model", "Canon EOS 300D DIGITAL").
+                            metadata("metadata.sensorWidth", 3152).
+                            metadata("metadata.sensorHeight", 2068).
+                            metadata("metadata.sensorLeftBorder", 72).
+                            metadata("metadata.sensorTopBorder", 16).
+                            metadata("metadata.sensorRightBorder", 3143).
+                            metadata("metadata.sensorBottomBorder", 2063).
+                            metadata("metadata.focalLength", 28.0f).
+                            metadata("metadata.serialNumber", 1330526302).
+                            metadata("metadata.decoderTable", new int[] {0, 0, 0x202, 0x52B336}).
+                            metadata("metadata.whiteBalance", 0).
+                            metadata("metadata.whiteBalanceAsString", "auto").
+                            metadata("metadata.RBCoefficients", new double[] { 1.9963898916967509, 1.1778846153846154}).
+                            metadata("metadata.colorTemperatureAvailable", true).
+                            metadata("metadata.colorTemperature", 5200)         );
       }
   }
