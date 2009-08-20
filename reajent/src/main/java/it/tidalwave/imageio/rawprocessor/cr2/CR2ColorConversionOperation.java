bc05ada03ec10cabd0218c200b366e2eb46e9553
Fabrizio Giudici <Fabrizio.Giudici@tidalwave.it>
2009-08-20 13:44:32 +0200

    Fix for JRW-248.

--- reajent/src/main/java/it/tidalwave/imageio/rawprocessor/cr2/CR2ColorConversionOperation.java
+++ reajent/src/main/java/it/tidalwave/imageio/rawprocessor/cr2/CR2ColorConversionOperation.java
@@ -51,15 +51,29 @@ public class CR2ColorConversionOperation extends ColorConversionOperation
 
     public CR2ColorConversionOperation()
       {
-        matrixMapByModel.put("CANON EOS 350D DIGITAL", get(new int[] { 6018,-617,-965,-8645,15881,2975,-1530,1719,7642 }));
+        // FIXME: put in property file
+        matrixMapByModel.put("CANON EOS-1D",           get(new int[] { 6806,-179,-1020,-8097,16415,1687,-3267,4236,7690 }));
+        matrixMapByModel.put("CANON EOS-1D MARK II",   get(new int[] { 6264,-582,-724,-8312,15948,2504,-1744,1919,8664 }));
+        matrixMapByModel.put("CANON EOS-1D MARK II N", get(new int[] { 6240,-466,-822,-8180,15825,2500,-1801,1938,8042 }));
+        matrixMapByModel.put("CANON EOS-1D MARK III",  get(new int[] { 6291,-540,-976,-8350,16145,2311,-1714,1858,7326 }));
+        matrixMapByModel.put("CANON EOS-1DS",          get(new int[] { 4374,3631,-1743,-7520,15212,2472,-2892,3632,8161 }));
+        matrixMapByModel.put("CANON EOS-1DS MARK II",  get(new int[] { 6517,-602,-867,-8180,15926,2378,-1618,1771,7633 }));
+        matrixMapByModel.put("CANON EOS-1DS MARK III", get(new int[] { 5859,-211,-930,-8255,16017,2353,-1732,1887,7448 }));
+        matrixMapByModel.put("CANON EOS 5D",           get(new int[] { 6347,-479,-972,-8297,15954,2480,-1968,2131,7649 }));
+        matrixMapByModel.put("CANON EOS 5D MARK II",   get(new int[] { 4716,603,-830,-7798,15474,2480,-1496,1937,6651 }));
         matrixMapByModel.put("CANON EOS 10D DIGITAL",  get(new int[] { 8197,-2000,-1118,-6714,14335,2592,-2536,3178,8266  }));
-        matrixMapByModel.put("CANON EOS 20Da DIGITAL", get(new int[] { 14155,-5065,-1382,-6550,14633,2039,-1623,1824,6561 }));
-        matrixMapByModel.put("CANON EOS 20D DIGITAL",  get(new int[] { 6599,-537,-891,-8071,15783,2424,-1983,2234,7462 }));
-        matrixMapByModel.put("CANON EOS 30D DIGITAL",  get(new int[] { 6257,-303,-1000,-7880,15621,2396,-1714,1904,7046 }));
-        matrixMapByModel.put("CANON EOS 40D DIGITAL",  get(new int[] { 6071,-747,-856,-7653,15365,2441,-2025,2553,7315 }));
-        matrixMapByModel.put("CANON EOS 50D DIGITAL",  get(new int[] { 4920,616,-593,-6493,13964,2784,-1774,3178,7005 }));
+        matrixMapByModel.put("CANON EOS 20Da",         get(new int[] { 14155,-5065,-1382,-6550,14633,2039,-1623,1824,6561 }));
+        matrixMapByModel.put("CANON EOS 20D",          get(new int[] { 6599,-537,-891,-8071,15783,2424,-1983,2234,7462 }));
+        matrixMapByModel.put("CANON EOS 30D",          get(new int[] { 6257,-303,-1000,-7880,15621,2396,-1714,1904,7046 }));
+        matrixMapByModel.put("CANON EOS 40D",          get(new int[] { 6071,-747,-856,-7653,15365,2441,-2025,2553,7315 }));
+        matrixMapByModel.put("CANON EOS 50D",          get(new int[] { 4920,616,-593,-6493,13964,2784,-1774,3178,7005 }));
+        matrixMapByModel.put("CANON EOS 350D DIGITAL", get(new int[] { 6018,-617,-965,-8645,15881,2975,-1530,1719,7642 }));
+        matrixMapByModel.put("CANON EOS 400D DIGITAL", get(new int[] { 7054,-1501,-990,-8156,15544,2812,-1278,1414,7796 }));
+        matrixMapByModel.put("CANON EOS 450D",         get(new int[] { 5784,-262,-821,-7539,15064,2672,-1982,2681,7427 }));
+        matrixMapByModel.put("CANON EOS 1000D",        get(new int[] { 6771,-1139,-977,-7818,15123,2928,-1244,1437,7533 }));
       }
 
+    @Nonnull
     private static ColorMatrix get (final @Nonnull int[] values)
       {
         return getMatrix(values, 1.0/10000.0);
@@ -75,8 +89,11 @@ public class CR2ColorConversionOperation extends ColorConversionOperation
     protected ColorMatrix getColorMatrixXYZ (final @Nonnull PipelineArtifact artifact)
       {
         final String model = ((TIFFMetadataSupport)artifact.getRAWMetadata()).getPrimaryIFD().getModel().toUpperCase().trim();
+        final ColorMatrix matrix = matrixMapByModel.get(model);
         // FIXME: try to use the embedded matrix instead of hardwired coefficients.
 
-        return matrixMapByModel.get(model);
+        logger.fine("getColorMatrixXYZ(%s) - model: %s, returning %s", artifact, model, matrix);
+
+        return matrix;
       }    
   }
--- reajent/src/test/java/it/tidalwave/imageio/rawprocessor/cr2/CR2ProcessorTest.java
+++ reajent/src/test/java/it/tidalwave/imageio/rawprocessor/cr2/CR2ProcessorTest.java
@@ -54,95 +54,95 @@ public class CR2ProcessorTest extends NewImageReaderTestSupport
           (
             // EOS 1D mkII
             ExpectedResults.create("http://www.rawsamples.ch/raws/canon/1dm2/RAW_CANON_1DM2.CR2").
-                            image(3504, 2336, 3, 8, "df74ee076c0d6af72d304cce7a5c3fe3").
+                            image(3504, 2336, 3, 8, "4fd1614b374c437fcedb66b6576a24dd").
                             thumbnail(1536, 1024).
                             thumbnail(160, 120).
                             thumbnail(384, 256).
-                            issues("JRW-233"),
+                            issues("JRW-233", "JRW-248"),
             // EOS 1D mkII N
             ExpectedResults.create("http://www.rawsamples.ch/raws/canon/1dm2n/RAW_CANON_1DM2N.CR2").
-                            image(3504, 2336, 3, 8, "9220a0897c7f19a5e9ec8abad906ed29").
+                            image(3504, 2336, 3, 8, "9122989c82f73a2a069dca14437f8c3b").
                             thumbnail(1728, 1152).
                             thumbnail(160, 120).
                             thumbnail(384, 256).
-                            issues("JRW-233"),
+                            issues("JRW-233", "JRW-248"),
             // EOS 1D mkIII
             ExpectedResults.create("http://www.rawsamples.ch/raws/canon/1dm3/RAW_CANON_1DMARK3.CR2").
-                            image(3888, 2592, 3, 8, "e59fcbeb0fcd2412c7cead7ce6f5b989").
+                            image(3888, 2592, 3, 8, "72693102fc1d49a67f603c2a2569a077").
                             thumbnail(1936, 1288).
                             thumbnail(160, 120).
                             thumbnail(486, 324).
-                            issues("JRW-233", "JRW-238"),
+                            issues("JRW-233", "JRW-238", "JRW-248"),
             // EOS 1Ds mkII
             ExpectedResults.create("http://www.rawsamples.ch/raws/canon/1dsm2/RAW_CANON_1DSM2.CR2").
-                            image(4992, 3328, 3, 8, "c5682d2fa8ef55ab67bb534553bf0a34").
+                            image(4992, 3328, 3, 8, "8c99e77e527d2c4cf117855635c63ed5").
                             thumbnail(1536, 1024).
                             thumbnail(160, 120).
                             thumbnail(384, 256).
-                            issues("JRW-233", "JRW-239"),
+                            issues("JRW-233", "JRW-239", "JRW-248"),
             // EOS 1Ds mkIII
             ExpectedResults.create("http://www.rawsamples.ch/raws/canon/1dsm3/RAW_CANON_1DSM3.CR2").
-                            image(5616, 3744, 3, 8, "8042146310a3f77db2f6c4c3f07165e3").
+                            image(5616, 3744, 3, 8, "952dbb135790933a06cd16ba3e3ffa2c").
                             thumbnail(2784, 1856).
                             thumbnail(160, 120).
                             thumbnail(476, 312).
-                            issues("JRW-233", "JRW-238"),
+                            issues("JRW-233", "JRW-238", "JRW-248"),
             // EOS 5D
             ExpectedResults.create("http://www.rawsamples.ch/raws/canon/5d/RAW_CANON_5D_ARGB.CR2").
-                            image(4368, 2912, 3, 8, "df60589bf48530258c67573790cf2cce").
+                            image(4368, 2912, 3, 8, "6ac13bff06588c76cc8a511376a62f24").
                             thumbnail(2496, 1664).
                             thumbnail(160, 120).
                             thumbnail(384, 256).
-                            issues("JRW-233", "JRW-239"),
+                            issues("JRW-233", "JRW-239", "JRW-248"),
             // EOS 50D
             ExpectedResults.create("http://www.rawsamples.ch/raws/canon/50d/RAW_CANON_50D.CR2").
-                            image(4752, 3168, 3, 8, "34d4d52b1ad0c8352b71690f460b6657").
+                            image(4752, 3168, 3, 8, "7e56badc2a14e995db9c5c0b11d40e4c").
                             thumbnail(4752, 3168).
                             thumbnail(160, 120).
                             thumbnail(604, 396).
-                            issues("JRW-237", "JRW-238", "JRW-239"),
+                            issues("JRW-237", "JRW-238", "JRW-239", "JRW-248"),
             // EOS 400D
             ExpectedResults.create("http://www.rawsamples.ch/raws/canon/400d/RAW_CANON_400D_ARGB.CR2").
-                            image(3888, 2592, 3, 8, "e96ed62b6d28a711f796dfe05211d4ec").
+                            image(3888, 2592, 3, 8, "5f64994bb5b5127d5d1410be7e85e6e6").
                             thumbnail(1936, 1288).
                             thumbnail(160, 120).
                             thumbnail(384, 256).
-                            issues(),
+                            issues("JRW-248"),
             // EOS 450D
             ExpectedResults.create("http://www.rawsamples.ch/raws/canon/450d/RAW_CANON_450D.CR2").
-                            image(4272, 2848, 3, 8, "2412544fffe8003aa03a32290d1b57fd").
+                            image(4272, 2848, 3, 8, "c8017a48cf5b2cde0bbb9b01459ea4df").
                             thumbnail(2256, 1504).
                             thumbnail(160, 120).
                             thumbnail(539, 356).
-                            issues("JRW-233", "JRW-238"),
+                            issues("JRW-233", "JRW-238", "JRW-248"),
             // EOS20D v1.1.0
             ExpectedResults.create("http://s179771984.onlinehome.us/RAWpository/images/canon/EOS20D/1.1.0/_MG_8587.CR2").
-                            image(3504, 2336, 3, 8, "77bd8c04f2895087aefbfb022bfd3b51").
+                            image(3504, 2336, 3, 8, "a2c3ccfb292c6c3feaa4d307992468b2").
                             thumbnail(1536, 1024).
                             thumbnail(160, 120).
                             thumbnail(384, 256).
-                            issues("JRW-208", "JRW-216", "JRW-217"),
+                            issues("JRW-208", "JRW-216", "JRW-217", "JRW-248"),
             // EOS20D v2.0.3
             ExpectedResults.create("http://s179771984.onlinehome.us/RAWpository/images/canon/EOS20D/2.0.3/XXXX0000.CR2").
-                            image(3504, 2336, 3, 8, "6200fba42ece0cced0604e41226384b0").
+                            image(3504, 2336, 3, 8, "d03c4f65666cd3f1b87593ecfa8883dc").
                             thumbnail(1536, 1024).
                             thumbnail(160, 120).
                             thumbnail(384, 256).
-                            issues("JRW-208", "JRW-216", "JRW-217"),
+                            issues("JRW-208", "JRW-216", "JRW-217", "JRW-248"),
             // EOS30D v1.0.4
             ExpectedResults.create("http://s179771984.onlinehome.us/RAWpository/images/canon/EOS30D/1.0.4/_MG_8882.CR2").
-                            image(3504, 2336, 3, 8, "d8cad9ef2f3cb8b3e82f0529057ff6e3").
+                            image(3504, 2336, 3, 8, "1c0f91691a5babeae072637fabc90ad1").
                             thumbnail(1728, 1152).
                             thumbnail(160, 120).
                             thumbnail(384, 256).
-                            issues("JRW-208", "JRW-216", "JRW-217"),
+                            issues("JRW-208", "JRW-216", "JRW-217", "JRW-248"),
             // EOS40D v1.0.3
             ExpectedResults.create("http://s179771984.onlinehome.us/RAWpository/images/canon/EOS40D/1.0.3/img_0003.cr2").
-                            image(3888, 2592, 3, 8, "c34d655dadc43c5a4102288b60ec49c2") .
+                            image(3888, 2592, 3, 8, "837f1d34968e8298b8fd5499c5767041") .
                             thumbnail(1936, 1288).
                             thumbnail(160, 120).
                             thumbnail(486, 324).
-                            issues("JRW-208", "JRW-216", "JRW-217", "JRW-238"),
+                            issues("JRW-208", "JRW-216", "JRW-217", "JRW-238", "JRW-248"),
             // EOS350D
             ExpectedResults.create("https://imaging.dev.java.net/nonav/TestSets/others/marioivankovits/Canon/EOS305D/CR2/IMG_4707.CR2").
                             image(3456, 2304, 3, 8, "d502b9e00b3d7507b8d39da9ec612d07").
