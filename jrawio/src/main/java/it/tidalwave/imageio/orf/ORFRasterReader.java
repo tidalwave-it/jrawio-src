6ba98c2ccd6579ec074e0f57bdb1d42c0ec59385
Fabrizio Giudici <Fabrizio.Giudici@tidalwave.it>
2009-08-22 00:12:33 +0200

    Fix for JRW-236.

--- jrawio/src/main/java/it/tidalwave/imageio/orf/ORFRasterReader.java
+++ jrawio/src/main/java/it/tidalwave/imageio/orf/ORFRasterReader.java
@@ -31,6 +31,7 @@ import javax.annotation.Nonnull;
 import java.util.HashMap;
 import java.util.Map;
 import it.tidalwave.imageio.raw.RasterReader;
+import it.tidalwave.imageio.util.Logger;
 
 /***********************************************************************************************************************
  *
@@ -42,23 +43,26 @@ import it.tidalwave.imageio.raw.RasterReader;
  **********************************************************************************************************************/
 public class ORFRasterReader extends RasterReader
   {
+    private final static String CLASS = ORFRasterReader.class.getName();
+    private final static Logger logger = Logger.getLogger(CLASS);
+
     private static final CSeriesRasterReader C_SERIES_RASTER_READER = new CSeriesRasterReader();
     private static final E300RasterReader E300_RASTER_READER = new E300RasterReader();
     private static final E410RasterReader E410_RASTER_READER = new E410RasterReader();
     private static final SPRasterReader SP_RASTER_READER = new SPRasterReader();
     
-    private static final Map<String, RasterReader> rasterReaderMapByModel = new HashMap<String, RasterReader>();
+    private static final Map<String, RasterReader> RASTER_READER_MAP_BY_MODEL = new HashMap<String, RasterReader>();
     
     static
       {
-        rasterReaderMapByModel.put("E-300", E300_RASTER_READER);
-        rasterReaderMapByModel.put("E-410", E410_RASTER_READER);
-        rasterReaderMapByModel.put("E-500", E300_RASTER_READER);
-        rasterReaderMapByModel.put("E-510", E410_RASTER_READER);
-
-        rasterReaderMapByModel.put("C5050Z",  SP_RASTER_READER);
-        rasterReaderMapByModel.put("SP350",   SP_RASTER_READER);
-        rasterReaderMapByModel.put("SP500UZ", SP_RASTER_READER);
+        RASTER_READER_MAP_BY_MODEL.put("E-300",   E300_RASTER_READER);
+        RASTER_READER_MAP_BY_MODEL.put("E-410",   E410_RASTER_READER);
+        RASTER_READER_MAP_BY_MODEL.put("E-500",   E300_RASTER_READER);
+        RASTER_READER_MAP_BY_MODEL.put("E-510",   E410_RASTER_READER);
+        RASTER_READER_MAP_BY_MODEL.put("C5050Z",  SP_RASTER_READER);
+        RASTER_READER_MAP_BY_MODEL.put("SP350",   SP_RASTER_READER);
+        RASTER_READER_MAP_BY_MODEL.put("SP500UZ", SP_RASTER_READER);
+        RASTER_READER_MAP_BY_MODEL.put("E-3",     E410_RASTER_READER);
       }
     
     /*******************************************************************************************************************
@@ -72,8 +76,10 @@ public class ORFRasterReader extends RasterReader
     @Nonnull
     public static RasterReader getInstance (@Nonnull String model)
       {
+        logger.fine("getInstance(%s)", model);
+        
         model = model.toUpperCase().trim();
-        final RasterReader rasterReader = rasterReaderMapByModel.get(model);
+        final RasterReader rasterReader = RASTER_READER_MAP_BY_MODEL.get(model);
         
         if (rasterReader != null)
           {
