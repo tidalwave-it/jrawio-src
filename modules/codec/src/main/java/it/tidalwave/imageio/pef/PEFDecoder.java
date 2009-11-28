d4de5fafcbea7c6da7eb00d3203df7bb83d1f279
Fabrizio Giudici <Fabrizio.Giudici@tidalwave.it>
2009-11-28 18:13:12 +0100

    Rolled back the previous change since it fails the test.

--- modules/codec/src/main/java/it/tidalwave/imageio/pef/PEFDecoder.java
+++ modules/codec/src/main/java/it/tidalwave/imageio/pef/PEFDecoder.java
@@ -96,7 +96,8 @@ public class PEFDecoder
         int value;
 
         // TODO: RAWImageInputStream supports zeroAfterFF - use it and simplify this loop
-        while (!reset && (availableBitCount < bitCount) && ((value = iis.read() & 0xff) != 0xff) &&
+        //while (!reset && (availableBitCount < bitCount) && ((value = iis.read() & 0xff) != 0xff) && // more correct, but breaks
+        while (!reset && (availableBitCount < bitCount) && ((value = iis.read() & 0xff) != -1) &&
                !(reset = zeroAfterFF && (value == 0xff) && (iis.read() != 0)))
           {
             bitBuffer = (bitBuffer << 8) | value; // (uchar)c
